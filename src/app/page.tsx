b1a95a875e1c807471d9b1b42f0de60f1afe6bdc
sahil kushwaha <sahilkushwaha5510@gmail.com>
2024-08-09 11:50:26 +0530

    add testimonials component or section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import Testimonials from "@/components/TestimonialCard";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
        <HeroSection />
        <FeaturedCourses/>
        <WhyChooseUs/>
+       <Testimonials />
     </main>
   );
 }
