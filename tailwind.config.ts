5d1bc75a780ff854129769c92bccead665650008
sahil kushwaha <sahilkushwaha5510@gmail.com>
2024-08-25 14:14:36 +0530

    contact us page added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,6 +52,9 @@ const config: Config = {
   darkMode:"class",
   theme: {
     extend: {
+      backgroundColor:{
+         "navy-blue":"#00061c"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -60,6 +63,7 @@ const config: Config = {
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
         scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+        "meteor-effect": "meteor 5s linear infinite",
       },
       keyframes: {
         scroll: {
@@ -77,6 +81,14 @@ const config: Config = {
             transform: "translate(-50%,-40%) scale(1)",
           },
         },
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        }
       },
     },
   },
